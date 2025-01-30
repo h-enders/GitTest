11b3532150906f530af60c5aa966bad4b9b2225d
hunterenders <hunterenders@gmail.com>
2025-01-30 16:40:20 -0600

    Added purpose for program

--- GitTest.cpp
+++ GitTest.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout << "Hello, world!" << endl;
     cout << "My name is Hunter, and I like to code!" << endl;
+    cout << "I am working on using Git to control versions of my coding projects."
     cout << "This is version 2 of 'GitTest.cpp'" << endl;
 
     return 0;
