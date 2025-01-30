0c99802cbcd92adea19b427f92fe1eac3c93bd4f
hunterenders <hunterenders@gmail.com>
2025-01-30 16:38:25 -0600

    Init build

--- GitTest.cpp
+++ GitTest.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
     cout << "Hello, world!" << endl;
-    cout << "This is version 1 of 'GitTest.cpp'" << endl;
+    cout << "My name is Hunter, and I like to code!" << endl;
+    cout << "This is version 2 of 'GitTest.cpp'" << endl;
 
     return 0;
 }
